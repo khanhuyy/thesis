50224f7a2ecb8ae1a2391bb5792e397299db053d
huypk <huypk239@gmail.com>
2023-10-26 06:58:49 +0700

    26-10 morning home

--- src/component/singlePage/CreateProduct.jsx
+++ src/component/singlePage/CreateProduct.jsx
@@ -2,28 +2,56 @@ import axios from 'axios'
 import React, { useEffect, useState } from 'react'
 import SinglePageGrid from './SinglePageGrid'
 import "../../CSS/SingleProduct.css"
-import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Input, InputGroup, Stack,Textarea,useToast } from '@chakra-ui/react'
+import { Breadcrumb, BreadcrumbItem, BreadcrumbLink, Input, InputGroup, Stack,Text,Textarea,useToast } from '@chakra-ui/react'
 import { useParams } from 'react-router-dom'
 import Nav from '../Nav'
 import Footer from '../footer/Footer'
-import {collection, onSnapshot, query, where, getDoc, doc } from "firebase/firestore";
+import { baseUrl } from '../../Url'
+
 const CreateProduct = () => {
     const [categories, setCategories] = useState();
     const [attributes, setAttributes] = useState();
 
+    const fetchAttribute = () => {
+        axios.get(`${baseUrl}/cartItems?cartID=1`)
+          .then((doc) => {
+            setAttributes(doc.data);
+          })
+          .catch((err) => console.log(err))
+      }
+    useEffect(() => {
+    fetchAttribute()
+    }, [])
+
+    const handleQty = (e) => {
+
+        const qty = e.target.value;
+        setQty(qty)
+
+        dispatch(updateCartProductQty(id,+qty,Price ,ogPrice))
+        .then(()=>{dispatch(getCartProducts)})
+
+    }
+
     return (
         <div>
             <Nav />
-            <InputGroup>
+            <Stack>
+                <Text>Name</Text>
                 <Input placeholder='Name'/>
                 <Textarea>Name</Textarea>
                 <Textarea>Category</Textarea>
-                <Textarea>Attribute</Textarea>
+                <Text>Attribute</Text>
+                <Select  variant="flushed" value={qty} onChange={ handleQty } textAlign={'center'} maxW={"max-content"}   >
+                    <option value= {1}>1</option>
+                    <option value= {2}>2</option>
+                    <option value= {3}>3</option>
+                </Select>
                 <Textarea>Price</Textarea>
                 <Textarea></Textarea>
                 <Textarea></Textarea>
                 <Textarea></Textarea>
-            </InputGroup>
+            </Stack>
             
             <Footer />
         </div>
